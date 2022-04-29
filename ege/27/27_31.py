5c8f890d4afed9bcc14fb4c80eb1cb1307dc3e0b
ramilevna <96003400+ramilevna@users.noreply.github.com>
2022-04-29 23:03:30 +0300

    Update 27_31.py

--- ege/27/27_31.py
+++ ege/27/27_31.py
@@ -1,16 +1,23 @@
-f = open('27-A.txt','r')
+f = open('C:\\Users\\renat\\Downloads\\27-B.txt','r')
 n = int(f.readline())
-arr = [0]*30
+ost = [0]*30
 s = 0
 k = 0
 mx = 0
 for i in range(n):
     a = int(f.readline())
     s += a
-    if a > 0 and abs(a) % 2 == 0:
+    if a % 2 == 0 and a > 0:
         k += 1
-    if a > 0 and a % 2 == 0 and k < 30:
-        arr[k]
+    if k % 30 == 0:
+        mx = max(s, mx)
+    if not(ost[k % 30]):
+        ost[k % 30] = s
+    else:
+        mx = max(mx, s - ost[k % 30])
+        ost[k % 30] = min(s, ost[k % 30])
+print(mx)
+
 
 
 # Дана последовательность целых чисел. Необходимо найти максимально возможную сумму её непрерывной
